49fc9dfb8974676134171835c104aee6a01695f9
beshubh <be.shubh42@gmail.com>
2026-02-15 20:31:20 +0530

    heap problems

---
+++ problems/heap/kth_closest_to_origin.py
@@ -0,0 +1,20 @@
+import math
+import heapq
+
+
+class Solution:
+    def euclidean_dist(self, p1, p2) -> float:
+        return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        p0 = (0, 0)
+        distances = []
+        for pi in points:
+            dist = self.euclidean_dist(p0, pi)
+            distances.append((dist, pi))
+        heapq.heapify(distances)
+        res = []
+        while k > 0:
+            res.append(heapq.heappop(distances)[1])
+            k -= 1
+        return res
