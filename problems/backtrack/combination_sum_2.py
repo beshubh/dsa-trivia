e0ee7967ae2379cbeb58679eb7d4e18e05d5456e
beshubh <be.shubh42@gmail.com>
2026-02-20 09:33:11 +0530

    more backtracking

--- problems/backtrack/combination_sum_2.py
+++ problems/backtrack/combination_sum_2.py
@@ -1,22 +1,29 @@
 class Solution:
     def combinationSum2(self, candidates: list[int], target: int) -> list[list[int]]:
-        res = set()
+        res = []
+        # sort the array so same elements are adjacent
+        # which will allows us to skip the elements that are the same as the one we choose to include into our result set
+        candidates = sorted(candidates)
 
         def dfs(i = 0, cur_sum = 0, cur=[]):
             if cur_sum > target:
                 cur.pop()
                 return
             if cur_sum == target:
-                res.add(tuple(sorted(cur)))
+                res.append(cur)
                 return
 
             if i >= len(candidates):
                 if cur_sum == target:
-                    res.add(sorted(tuple(cur)))
+                    res.append(cur)
                 return
             # include
             dfs(i + 1, cur_sum + candidates[i], cur + [candidates[i]])
+            # skip all the elements that are similar to the one we chose to include
+            while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
+                i += 1
             # exclude
             dfs(i + 1, cur_sum, cur)
+
         dfs()
         return [list(x) for x in res]
---
+++ problems/backtrack/generate_parenthesis.py
@@ -0,0 +1,18 @@
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
+        result = []
+        def backtrack(open: int = 0, close: int = 0, current = []):
+            if close > open or open > n:
+                return
+
+            if open == close  and close == n:
+                result.append(''.join(current))
+                return
+
+            backtrack(open + 1, close, current + ['('])
+            if open > close:
+                backtrack(open, close + 1, current + [')'])
+        backtrack()
+        return result
